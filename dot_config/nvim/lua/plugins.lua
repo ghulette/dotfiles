d1c9c481841595f64127a9d033d4b4e2c956ac1d
Geoff Hulette <geoff@hulette.net>
2025-02-21 06:01:09 -0800

    Update

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -29,9 +29,19 @@ require("lazy").setup({
     "bohlender/vim-smt2",
     "nvim-tree/nvim-tree.lua",
     "nvim-tree/nvim-web-devicons",
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    }
 })
 
 require("nvim-tree").setup()
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', {
     noremap = true
 })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
