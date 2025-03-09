c5c7e734de180c25b3fd1e2d038f710f61e8403d
Geoff Hulette <geoff@hulette.net>
2025-03-09 13:55:54 -0700

    Update

--- dot_config/nvim/lua/themes.lua
+++ dot_config/nvim/lua/themes.lua
@@ -2,4 +2,8 @@
 require("catppuccin").setup()
 
 --set the colorscheme to it!
-vim.cmd.colorscheme "catppuccin"
+--vim.cmd.colorscheme "catppuccin"
+--vim.cmd.colorscheme "catppuccin-latte"
+vim.cmd.colorscheme "catppuccin-frappe"
+--vim.cmd.colorscheme "catppuccin-macchiato"
+--vim.cmd.colorscheme "catppuccin-mocha"
