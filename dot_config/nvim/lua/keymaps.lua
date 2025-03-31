87593fdb1ddd7f893126ae1aa9448d6fe48a2905
Geoff Hulette <geoff@hulette.net>
2025-03-31 16:10:43 -0700

    Added Ctrl-hjkl to move between splits

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -8,16 +8,15 @@ local opts = {
 -- Normal mode --
 -----------------
 
--- Resize with arrows
--- delta: 2 lines
-vim.keymap.set('n', '<C-Up>',    ':resize +2<CR>', opts)
-vim.keymap.set('n', '<C-Down>',  ':resize -2<CR>', opts)
-vim.keymap.set('n', '<C-Left>',  ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
-
 -- Move text up and down
 vim.keymap.set("n", "<A-J>", ":m .+1<CR>==", opts)
 vim.keymap.set("n", "<A-K>", ":m .-2<CR>==", opts)
+
+-- Move between splits with Ctrl-hjkl
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 
 -----------------
 -- Visual mode --
