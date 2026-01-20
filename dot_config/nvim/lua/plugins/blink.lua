10fce06b8eb6c5b2761576af9992e256f91389e6
Geoff Hulette <ghulette@amazon.com>
2026-01-20 14:33:23 -0800

    Update

---
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,15 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = {
+        auto_show = true,
+        auto_show_delay_ms = 500, -- Delay in ms
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500, -- Delay in ms
+      },
+    },
+  },
+}
