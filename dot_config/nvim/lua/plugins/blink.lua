c8a48c59ba200c84b44799879864ab26501380f4
Geoff Hulette <ghulette@amazon.com>
2026-02-10 09:19:37 -0800

    Turn off auto autocomplete

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -3,11 +3,11 @@ return {
   opts = {
     completion = {
       menu = {
-        auto_show = true,
+        auto_show = false,
         auto_show_delay_ms = 500, -- Delay in ms
       },
       documentation = {
-        auto_show = true,
+        auto_show = false,
         auto_show_delay_ms = 500, -- Delay in ms
       },
     },
