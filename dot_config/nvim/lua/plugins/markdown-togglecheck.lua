94c214844b58cf0e6c447d06290ea1e0865af800
Geoff Hulette <ghulette@amazon.com>
2026-04-21 13:36:35 -0700

    Toggle checkbox

---
+++ dot_config/nvim/lua/plugins/markdown-togglecheck.lua
@@ -0,0 +1,24 @@
+-- Checkbox toggle for markdown, no treesitter dependency.
+local function toggle_check()
+  local line = vim.api.nvim_get_current_line()
+  local new
+  if line:match("%- %[ %]") then
+    new = line:gsub("%- %[ %]", "- [x]", 1)
+  elseif line:match("%- %[x%]") then
+    new = line:gsub("%- %[x%]", "- [ ]", 1)
+  elseif line:match("^%s*%- ") then
+    new = line:gsub("^(%s*%- )", "%1[ ] ", 1)
+  else
+    return
+  end
+  vim.api.nvim_set_current_line(new)
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function(ev)
+    vim.keymap.set("n", "<leader>nn", toggle_check, { buffer = ev.buf, desc = "Toggle checkbox" })
+  end,
+})
+
+return {}
